8d5072a7556ba5f91544d2b91fc08bb30a04d32c
Drew Hartberg <drewhartberg@gmail.com>
2019-10-04 16:59:36 +0000

    Finished

--- image-blur.rb
+++ image-blur.rb
@@ -1,17 +1,24 @@
+#require 'byebug'
+
 class Image
 
-	def initialize(value)
-		@value = value
+	def initialize(arg_passed_in)
+		@value = arg_passed_in
 	end
+
 
 	def output_image
+		@value.each do |sub|
+			puts sub.join
+		end
 	end
 end
+#debugger
+image = Image.new([
+		[0, 0, 0, 0],
+		[0, 1, 0, 0],
+		[0, 0, 0, 1],
+		[0, 0, 0, 0]
+	])
 
-image = Image.new(
-	[0, 0, 0, 0],
-	[0, 1, 0, 0],
-	[0, 0, 0, 1],
-	[0, 0, 0, 0]
-)
-@image.output_image
+image.output_image
