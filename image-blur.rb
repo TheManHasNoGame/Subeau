e49b06dc2802dc01cb957475b9c039ce5e2e1130
Drew Hartberg <drewhartberg@gmail.com>
2019-11-01 20:08:53 +0000

    next problems

--- image-blur.rb
+++ image-blur.rb
@@ -1,19 +1,57 @@
-#require 'byebug'
-
 class Image
+  def initialize(rows)
+    @rows = rows
+  end
 
-	def initialize(arg_passed_in)
-		@value = arg_passed_in
-	end
-
-
-	def output_image
-		@value.each do |sub|
-			puts sub.join
-		end
-	end
+  def blur
+    @rows_copy = Array.new(@rows.size) {Array.new(@rows.first.size)}
+    @rows.each_with_index do |row, row_index|
+      row.each_with_index do |cell, column_index|
+        set_zero(row_index,column_index)
+      end
+    end
+    @rows.each_with_index do |row, row_index|
+      row.each_with_index do |cell, column_index|
+        blur_location(row_index,column_index)
+      end
+    end
+    @rows = @rows_copy
+  end
+  def set_zero(row_index, column_index)
+     @rows_copy[row_index][column_index] = 0
+  end
+  def blur_location (row_index, column_index)
+    if @rows[row_index][column_index] == 1
+      @rows_copy[row_index][column_index] = 1
+      if column_index == @rows_copy[row_index].size
+        next
+      else
+        @rows_copy[row_index + 1][column_index] = 1
+      end
+      if column_index == @rows_copy[row_index].size
+        next
+      else
+        @rows_copy[row_index - 1][column_index] = 1
+      end
+      if column_index == @rows_copy[row_index].size
+        next
+      else
+        @rows_copy[row_index][column_index + 1] = 1
+      end
+      if column_index == @rows_copy[row_index].size
+        next
+      else
+        @rows_copy[row_index][column_index - 1] = 1
+      end
+    end
+  end
+  def output_image
+    @rows.each_with_index do |row, row_index|
+      puts row.join
+    end
+  end
 end
-#debugger
+
 image = Image.new([
 		[0, 0, 0, 0],
 		[0, 1, 0, 0],
@@ -21,4 +59,5 @@ image = Image.new([
 		[0, 0, 0, 0]
 	])
 
+image.blur
 image.output_image
