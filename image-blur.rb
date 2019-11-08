c18e8f3915dd7000b875e33dce8e5e7737400414
Drew Hartberg <drewhartberg@gmail.com>
2019-11-08 19:18:24 +0000

    Image-Blur Title

--- image-blur.rb
+++ image-blur.rb
@@ -6,45 +6,6 @@ class Image
     @rows = rows
   end
 
-  def blur
-    @rows_copy = Array.new(@rows.size) {Array.new(@rows.first.size)}
-    @rows.each_with_index do |row, row_index|
-      row.each_with_index do |cell, column_index|
-        @rows_copy[row_index][column_index] = 0
-      end
-    end
-
-    @rows.each_with_index do |row, row_index|
-# debugger
-      row.each_with_index do |cell, column_index|
-        blur_location(row_index,column_index)
-      end
-    end
-    @rows = @rows_copy
-  end
-
-  def blur_location (row_index, column_index)
-    if @rows[row_index][column_index] == 1
-        @rows_copy[row_index][column_index] = 1
-      #checks below
-      if row_index != @rows_copy[row_index].size - 1
-         @rows_copy[row_index + 1][column_index] = 1
-      end
-      #checks above
-      if row_index != 0
-        @rows_copy[row_index - 1][column_index] = 1
-      end
-      # checks to the right
-      if column_index != @rows_copy[row_index].size - 1
-        @rows_copy[row_index][column_index + 1] = 1
-      end
-      #checks to the left
-      if column_index != 0
-        @rows_copy[row_index][column_index - 1] = 1
-      end
-    end
-  end
-
   def output_image
     @rows.each do |row|
       puts row.join
@@ -53,11 +14,10 @@ class Image
 end
 
 image = Image.new([
-		[1, 0, 0, 1],
 		[0, 0, 0, 0],
-		[0, 0, 0, 0],
-		[1, 0, 0, 1]
+		[0, 1, 0, 0],
+		[0, 0, 0, 1],
+		[0, 0, 0, 0]
 	])
 
-image.blur
 image.output_image
